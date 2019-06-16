af3a374d6f577aaf089ad021d95407246a232396
JJunJang <JJunJang1280@naver.com>
2019-06-16 19:30:15 +0900

    local -> RDS server Change
    ㅈㄱㄴ

--- Python_DB/takealookDBcreate.py
+++ Python_DB/takealookDBcreate.py
@@ -1,6 +1,6 @@
 import pymysql
 
-connect = pymysql.connect(host='localhost', user='root', password='han1280', db='takealook', charset='utf8', local_infile = 1)
+connect = pymysql.connect(host='takealook.cjdwnzzk2agh.ap-northeast-2.rds.amazonaws.com', user='tal_admin', password='take1234', db='takealook', charset='utf8', local_infile = 1)
 cursor = connect.cursor(pymysql.cursors.DictCursor)
 
 # 'execute' 메소드에 for문을 이용하여 실행할 쿼리문을 전달하고 'fetchall' 메소드를 이용하여 쿼리문을 실행하였습니다.
